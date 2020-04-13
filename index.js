70bef6d6399f6e8464b240cd0ce0610a7446372c
Nattapong Sirilappanich <nattapong.sirilappanich2@ibm.com>
2020-04-13 14:35:34 +0700

    First revision

---
+++ index.js
@@ -0,0 +1,18 @@
+const Express = require("express")
+const path = require("path")
+
+let app = Express()
+const basePath = path.join(__dirname, "public")
+const PORT = process.env.PORT
+
+app.use(basePath)
+app.get("/ping", (_req, res) => {
+    res.send("pong")
+})
+app.get("/", (_req, res) => {
+    res.sendFile(path.join(basePath, "index.html"))
+})
+
+app.listen(PORT, () => {
+    console.log("App started on port", PORT)
+})
---
+++ public/build/precache-manifest.967ec89c02b334e6e71ebe361b81cf35.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f378118d1bbb630c1045a1df293119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3085479aef8fbc237808",
+    "url": "/static/css/main.8c489395.chunk.css"
+  },
+  {
+    "revision": "149e9acc34f37cd60fb4",
+    "url": "/static/js/2.01425107.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.01425107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3085479aef8fbc237808",
+    "url": "/static/js/main.bf1cb354.chunk.js"
+  },
+  {
+    "revision": "d952e6d0695ff294af8d",
+    "url": "/static/js/runtime-main.57633b7a.js"
+  },
+  {
+    "revision": "d9c2b1bda680cfe97f20c46c1033cf47",
+    "url": "/static/media/watson-avatar.d9c2b1bd.png"
+  },
+  {
+    "revision": "942da270ea39e9d0ce9df198b400563e",
+    "url": "/static/media/watsondark.942da270.gif"
+  },
+  {
+    "revision": "7ef19e7f28420e846d6dd475ee002f91",
+    "url": "/static/media/watsonlight.7ef19e7f.gif"
+  }
+]);
